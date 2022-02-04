074efa6e4d58cf4389a7367deb8951e8638c6f5a
John Lin <john.lin@mail.concordia.ca>
2022-02-03 20:39:01 -0500

    Automodified spacing in Driver

--- COMP346_A1/src/Driver.java
+++ COMP346_A1/src/Driver.java
@@ -11,23 +11,24 @@
  */
 public class Driver {
 
-    /** 
+    /**
      * main class
+     * 
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    	
-    	 /*******************************************************************************************************************************************
-    	  * TODO : implement all the operations of main class   																					*
-    	  ******************************************************************************************************************************************/
-        
-    	Network objNetwork = new Network("network");            /* Activate the network */
+
+        /*******************************************************************************************************************************************
+         * TODO : implement all the operations of main class *
+         ******************************************************************************************************************************************/
+
+        Network objNetwork = new Network("network"); /* Activate the network */
         objNetwork.start();
-        Server objServer = new Server();                        /* Start the server */ 
+        Server objServer = new Server(); /* Start the server */
         objServer.start();
-        Client objClient1 = new Client("sending");              /* Start the sending client */
+        Client objClient1 = new Client("sending"); /* Start the sending client */
         objClient1.start();
-        Client objClient2 = new Client("receiving");            /* Start the receiving client */
-         objClient2.start();
+        Client objClient2 = new Client("receiving"); /* Start the receiving client */
+        objClient2.start();
     }
 }
